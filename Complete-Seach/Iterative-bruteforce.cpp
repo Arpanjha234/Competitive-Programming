0799fe729cc5869c5526142464d719c5c6f074c9
Arpan Jha <62328473+Arpanjha234@users.noreply.github.com>
2024-03-19 03:08:16 +0530

    created Iterative-bruteforce.cpp

---
+++ Complete-Seach/Iterative-bruteforce.cpp
@@ -0,0 +1,7 @@
+// Time Complexity O(n^2) (can go upto O(n^k) as per number of states)
+// enumerating unique pairs of i & j where (i & j) < n
+for(int i = 0; i < n; i++){
+  for(int j = i + 1; j < n; j++){
+  
+  }
+}
