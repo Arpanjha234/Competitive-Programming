10a64e935f8fb1bfbb0b932f5e14620e9d90f81e
Arpan Jha <62328473+Arpanjha234@users.noreply.github.com>
2024-03-22 15:24:13 +0530

    added Increasing-array.cpp

---
+++ CSES/Introductory Problems/Increasing-array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // fast-io
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+ 
+  long long a[n];
+  for(int i = 0; i < n; i++){cin >> a[i];}
+ 
+  long long ans = 0, delta = 0;
+  for(int i = 0; i < n - 1; i++){
+    if(a[i] > a[i + 1]){
+      delta = (a[i] - a[i + 1]);
+      a[i + 1] += delta;
+      ans += delta;
+    }
+  }
+  
+  cout << ans << endl;
+  return 0;
+}
