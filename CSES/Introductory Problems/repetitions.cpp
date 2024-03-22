582f995cd9cdd4e29dfa58506b77d85b0e20957e
Arpan Jha <62328473+Arpanjha234@users.noreply.github.com>
2024-03-22 14:52:13 +0530

    added repetitions.cpp

---
+++ CSES/Introductory Problems/repetitions.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // fast-io
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  string s;
+  cin >> s;
+  
+  int ans = 0, cnt = 1;
+  for(int i = 0; i < int(s.size()); i++){
+    if(s[i] == s[i + 1]){cnt++;}
+    else{
+      ans = max(ans, cnt);
+      cnt = 1;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
