587014c998ac51c80c836bbeaf237e86ca4a5975
Arpan Jha <62328473+Arpanjha234@users.noreply.github.com>
2024-03-25 02:07:07 +0530

    added dice-combinations.cpp

---
+++ CSES/Dynamic Programming/dice-combinations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int mod = 1e9 + 7;
+
+int main() {
+  // fast-io
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+
+  int dp[n + 100] = {0};
+
+  // base-case
+  dp[0] = dp[1] = 1;
+  // dp[i] = number of ways to construct sum n
+  for(int i = 2; i <= n; i++){
+    for(int j = 1; j <= 6; j++){
+      if((i - j) >= 0){
+        dp[i] = ((dp[i - j] + dp[i]) % mod);
+      }
+    }
+  }
+  cout << dp[n] << endl;
+  return 0;
+}
