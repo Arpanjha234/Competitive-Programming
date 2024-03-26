ce64e8ef9ee20b6abe0218bbe7b350f8d011c642
Arpan Jha <62328473+Arpanjha234@users.noreply.github.com>
2024-03-27 00:24:10 +0530

    updated bit-strings.cpp

--- CSES/Introductory Problems/bit-strings.cpp
+++ CSES/Introductory Problems/bit-strings.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 const int mod = 1e9+7;
-
+ 
 int main() {
   // fast-io
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  long long n, ans = 1;
+ 
+  int n, ans = 1;
   cin >> n;
-
+ 
   for(int i = 0; i < n; i++){
-      ans = (ans * 2) % mod;
+      ans = ((ans % mod) * 2) % mod;
   }
   
   cout << ans << '\n';
