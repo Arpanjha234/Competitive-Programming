721e601acfcb7074461a32402c6b96295060a230
Arpan Jha <62328473+Arpanjha234@users.noreply.github.com>
2024-03-29 02:45:31 +0530

    updated parity-check.cpp

--- Library/Snippets/parity-check.cpp
+++ Library/Snippets/parity-check.cpp
@@ -1,3 +1,13 @@
+// Method 1 : Shorter Method
+// Time Complexity : O(1)
+// Note : slightly faster in runtime than other approaches
 bool isEven(long long x){
  return !(x & 1);
 }
+
+// Method 2 : Longer Method
+// Time Complexity : O(1)
+bool isEven(long long x){
+ return !(x % 2);
+}
+
